8837b044cf06c33a3f86183743c732d51e9a5ccc
owen107 <owenxu107@gmail.com>
2014-10-30 20:11:43 -0400

    Concatenated JavaScript Files

---
+++ builds/development/js/script.js
@@ -0,0 +1,3 @@
+function weila() {
+	alert("weila");
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat');
 
-gulp.task('log', function() {
-	gutil.log('Weila');
+var jsSources = [
+   'components/scripts/app.js',
+   "components/jquery-ui.js"
+];
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+	   .pipe(concat('script.js'))
+	   .pipe(gulp.dest('builds/development/js'))
 });
