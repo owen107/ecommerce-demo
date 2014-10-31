19a6ed7cab0ac9b61d4ee6f58852bafa40ee66d3
owen107 <owenxu107@gmail.com>
2014-10-30 23:03:56 -0400

    Added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass');
 
 var jsSources = [
    'components/scripts/app.js',
    "components/jquery-ui.js"
 ];
 
+var sassSources = ['components/sass/style.scss'];
+
 gulp.task('js', function() {
 	gulp.src(jsSources)
 	   .pipe(concat('script.js'))
+	   .pipe(browserify())
 	   .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: 'builds/development/images',
+      style: 'expanded',
+      require: ['susy', 'breakpoint']
+    }))
+    .on('error', gutil.log)
+   .pipe(gulp.dest('builds/development/css'))
 });
